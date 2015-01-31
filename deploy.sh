30b5ccea661f9289d82ab8929048e0b91322714d
tka <tkatka@gmail.com>
2015-01-31 21:26:57 +0800

    deploy  content

--- deploy.sh
+++ deploy.sh
@@ -19,6 +19,7 @@ git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
+git subtree push --prefix=public git@github.com:tka/tka.github.io gh-pages
 
 # Come Back
 cd ..
--- public/deploy.sh
+++ public/deploy.sh
@@ -20,5 +20,7 @@ git commit -m "$msg"
 # Push source and build repos.
 git push origin master
 
+git subtree push --prefix=public git@github.com:tka/tka.github.io gh-pages
+
 # Come Back
 cd ..
